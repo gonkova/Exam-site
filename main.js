8e36f645cb84fed80382c0d07c50337fc8c76355
Boryana Gonkova <36599664+gonkova@users.noreply.github.com>
2023-02-19 23:43:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function toggle() {
+    let nav = document.getElementById("nav");
+
+    if (nav.style.display == 'block') {
+        nav.style.display = 'none';
+    }
+    else {
+        nav.style.display = 'block';
+    }
+
+}
